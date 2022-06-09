2d23eee22341bee30fc5d2e4a5b81a5f936f76b7
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-09 14:40:02 +0700

    feat: Get all books name query

--- src/handlers/GetAllBookHandler.js
+++ src/handlers/GetAllBookHandler.js
@@ -1,14 +1,29 @@
 const books = require('../books')
 
-const GetAllBookHandler = () => ({
-	status: 'success',
-	data: {
-		books: books.map(({ id, name, publisher }) => ({
-			id,
-			name,
-			publisher,
-		})),
-	},
-})
+const GetAllBookHandler = request => {
+	const { name } = request.query
+
+	let filteredBooks = books
+
+	if (name) {
+		filteredBooks = books.filter(
+			book =>
+				// eslint-disable-next-line implicit-arrow-linebreak, comma-dangle
+				book.name.toLowerCase().includes(name.toLowerCase())
+			// eslint-disable-next-line function-paren-newline
+		)
+	}
+
+	return {
+		status: 'success',
+		data: {
+			books: filteredBooks.map(({ id, name: bookName, publisher }) => ({
+				id,
+				name: bookName,
+				publisher,
+			})),
+		},
+	}
+}
 
 module.exports = GetAllBookHandler
