9bbe7b41bf0419c5c32084e97883086de450918b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-09 13:54:01 +0700

    feat: Delete books

--- src/handler.js
+++ src/handler.js
@@ -182,9 +182,33 @@ const UpdateBookHandler = (request, hapi) => {
 	}
 }
 
+const DeleteBookHandler = (request, hapi) => {
+	const { bookId } = request.params
+
+	const index = books.findIndex(book => book.id === bookId)
+
+	if (index === -1) {
+		return hapi
+			.response({
+				status: 'fail',
+				message: 'Buku gagal dihapus. Id tidak ditemukan',
+			})
+
+			.code(404)
+	}
+
+	books.splice(index, 1)
+
+	return {
+		status: 'success',
+		message: 'Buku berhasil dihapus',
+	}
+}
+
 module.exports = {
 	AddBookHandler,
 	GetAllBookHandler,
 	GetBookDetailHandler,
 	UpdateBookHandler,
+	DeleteBookHandler,
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const {
 	GetAllBookHandler,
 	GetBookDetailHandler,
 	UpdateBookHandler,
+	DeleteBookHandler,
 } = require('./handler')
 
 const routes = [
@@ -25,6 +26,11 @@ const routes = [
 		method: 'PUT',
 		path: '/books/{bookId}',
 		handler: UpdateBookHandler,
+	},
+	{
+		method: 'DELETE',
+		path: '/books/{bookId}',
+		handler: DeleteBookHandler,
 	},
 ]
 
